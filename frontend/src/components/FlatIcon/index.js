172f8b4550274643f56818c2ab1501cfd99212e0
BaukaErenbay <mavlyd.saidov@gmail.com>
2022-12-02 17:02:53 +0600

    flat icon fixed size

--- frontend/src/components/FlatIcon/index.js
+++ frontend/src/components/FlatIcon/index.js
@@ -1,12 +1,17 @@
 import React, {useRef} from "react"
 import classnames from "classnames"
+import styles from "./index.module.scss"
 
 const FlatIcon = props => {
 	const {name, className, ...rest} = props
 	const root = useRef(null)
 
 	return (
-		<i ref={root} className={classnames("fi", name, className)} {...rest} />
+		<i
+			ref={root}
+			className={classnames("fi", name, styles.root, className)}
+			{...rest}
+		/>
 	)
 }
 
